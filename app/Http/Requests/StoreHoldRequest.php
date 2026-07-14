818b754ed56eb682cb90f7841a4e097ab1c02b0e
Ahmed-Mahmoudd <ahmed.mahmoud.sobhy03@gmail.com>
2026-07-14 17:13:24 +0300

    Add product_id existence validation to StoreHoldRequest

--- app/Http/Requests/StoreHoldRequest.php
+++ app/Http/Requests/StoreHoldRequest.php
@@ -20,6 +20,7 @@ class StoreHoldRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'product_id' => ['required', 'integer', 'exists:products,id'],
             'qty' => ['required', 'integer', 'min:1'],
         ];
     }
